48df9d1f01c2beaa0446a5750de722e50aaf1d5e
G-Wald <gauthier_manigand@hotmail.fr>
2024-01-11 23:19:39 +0100

    creation des deux app

---
+++ yourcaryourwarback/src/main/java/com/poc/yourcaryourwarback/YourcaryourwarbackApplication.java
@@ -0,0 +1,13 @@
+package com.poc.yourcaryourwarback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourcaryourwarbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourcaryourwarbackApplication.class, args);
+	}
+
+}
---
+++ yourcaryourwarback/src/test/java/com/poc/yourcaryourwarback/YourcaryourwarbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.yourcaryourwarback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourcaryourwarbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

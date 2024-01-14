19ba7cb596382b03362e8bf5fe8a0080b3f3969d
G-Wald <gauthier_manigand@hotmail.fr>
2024-01-14 17:33:53 +0100

    ajout fichier bdd

--- baseDeDonnées.sql
+++ baseDeDonnées.sql
@@ -1,21 +1,12 @@
 CREATE TABLE Users (
-    Id INT PRIMARY KEY AUTO_INCREMENT,
+     Id INT PRIMARY KEY AUTO_INCREMENT,
     Email VARCHAR(100) UNIQUE,
     Username VARCHAR(100) UNIQUE,
-    Password VARCHAR(255)
-);
-
-CREATE TABLE Reservations (
-    ReservationID INT PRIMARY KEY AUTO_INCREMENT,
-    UserID INT,
-    PickupLocation VARCHAR(255),
-    ReturnLocation VARCHAR(255),
-    PickupDateTime DATETIME,
-    ReturnDateTime DATETIME,
-    VehicleCategory VARCHAR(50),
-    TotalAmount DECIMAL(10, 2),
-    Status VARCHAR(20),
-    FOREIGN KEY (UserID) REFERENCES Users(Id)
+    Password VARCHAR(255),
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    DateOfBirth DATE,
+    Address VARCHAR(255)
 );
 
 CREATE TABLE RentalAgencies (
@@ -47,11 +38,28 @@ CREATE TABLE ChatMessages (
     FOREIGN KEY (ReceiverID) REFERENCES Users(Id)
 );
 
+CREATE TABLE Reservations (
+    ReservationID INT PRIMARY KEY AUTO_INCREMENT,
+    UserID INT,
+    RentalOfferID INT,
+    PickupLocation VARCHAR(255),
+    ReturnLocation VARCHAR(255),
+    PickupDateTime DATETIME,
+    ReturnDateTime DATETIME,
+    ModificationDeadline DATETIME,
+    VehicleCategory VARCHAR(50),
+    TotalAmount DECIMAL(10, 2),
+    Status VARCHAR(20),
+    FOREIGN KEY (UserID) REFERENCES Users(Id),
+    FOREIGN KEY (RentalOfferID) REFERENCES RentalOffers(OfferID)
+);
+
 CREATE TABLE Payments (
     PaymentID INT PRIMARY KEY AUTO_INCREMENT,
     ReservationID INT,
     Amount DECIMAL(10, 2),
     PaymentDateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    Status VARCHAR(20),
     FOREIGN KEY (ReservationID) REFERENCES Reservations(ReservationID)
 );
 
